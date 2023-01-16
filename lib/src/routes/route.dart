18f4ff286fd90bca5270e6f5fbd37226ab34b12f
Robin Hood <yoga3911@gmail.com>
2023-01-17 00:29:17 +0700

    add new route

--- lib/src/routes/route.dart
+++ lib/src/routes/route.dart
@@ -3,6 +3,7 @@ import 'package:test_nusantara/src/presentations/pages/auth/login.dart';
 import 'package:test_nusantara/src/presentations/pages/auth/register.dart';
 import 'package:test_nusantara/src/presentations/pages/book/home.dart';
 import 'package:test_nusantara/src/presentations/pages/book/insert_book.dart';
+import 'package:test_nusantara/src/presentations/pages/main_page.dart';
 import 'package:test_nusantara/src/presentations/pages/book/update_book.dart';
 
 class Routes {
@@ -10,14 +11,14 @@ class Routes {
 
   static const login = "/auth/login";
   static const register = "/auth/register";
-  static const home = "/home";
+  static const main = "/main";
   static const add = "/home/add";
   static const edit = "/home/edit";
 
   static final data = <String, WidgetBuilder>{
     login: (_) => const LoginPage(),
     register: (_) => const RegisterPage(),
-    home: (_) => const HomePage(),
+    main: (_) => const MainPage(),
     add: (_) => const AddBookPage(),
     edit: (_) => const EditBookPage(),
   };
